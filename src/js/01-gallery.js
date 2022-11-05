780da33a0de2b037fe0b8c8af3cdc1ba07030e41
Rostyslav <rossgaluzinskiy@gmail.com>
2022-11-05 21:43:39 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryPhotos = document.querySelector('.gallery');
+
+let items = galleryItems.map(({ preview, original, description }) => 
+`<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a></li>`
+).join('')
+
+
+galleryPhotos.innerHTML = items
+
+const lightbox = new SimpleLightbox('.gallery a', {  
+  captions: true,
+  captionPosition: 'bottom',
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+new SimpleLightbox('.gallery a', { 
+    captionsData: alt,
+    captionDelay: 200,
+});
