0856fcab8c428ddfacbfb01c71a9b17a2109f989
Rostyslav <rossgaluzinskiy@gmail.com>
2022-11-06 17:28:54 +0200

    3-task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputForm, 500))
+getText()
+const onForm = {}
+
+function onInputForm(evt) {
+    onForm[evt.target.name] = evt.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(onForm))
+}
+
+function getText(evt) {
+    const saveText = JSON.parse(localStorage.getItem("feedback-form-state"))
+
+    if (saveText) {
+        form.email.value = saveText.email
+        form.message.value = saveText.message
+    }
+
+}
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(evt) {
+    evt.preventDefault()
+    evt.currentTarget.reset()
+    localStorage.removeItem("feedback-form-state")
+    console.log(onForm)
+}
